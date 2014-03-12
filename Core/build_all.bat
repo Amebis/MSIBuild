bf9d5a74e89bb58808b1ac40eb4ac4248e584713
Simon Rozman <simon.rozman@amebis.si>
2014-03-12 10:36:27 +0000

    Pripravo namestitvenega paketa sem obrnil tako, da je izvorni jezik angleščina.

---
+++ Core/build_all.bat
@@ -0,0 +1,13 @@
+@echo off
+nmake.exe /ls All LANG=En CFG=Debug   PLAT=Win32
+nmake.exe /ls All LANG=It CFG=Debug   PLAT=Win32
+nmake.exe /ls All LANG=Sl CFG=Debug   PLAT=Win32
+nmake.exe /ls All LANG=En CFG=Debug   PLAT=x64
+nmake.exe /ls All LANG=It CFG=Debug   PLAT=x64
+nmake.exe /ls All LANG=Sl CFG=Debug   PLAT=x64
+nmake.exe /ls All LANG=En CFG=Release PLAT=Win32
+nmake.exe /ls All LANG=It CFG=Release PLAT=Win32
+nmake.exe /ls All LANG=Sl CFG=Release PLAT=Win32
+nmake.exe /ls All LANG=En CFG=Release PLAT=x64
+nmake.exe /ls All LANG=It CFG=Release PLAT=x64
+nmake.exe /ls All LANG=Sl CFG=Release PLAT=x64
---
+++ UI/clean_all.bat
@@ -0,0 +1,13 @@
+@echo off
+nmake.exe /ls Clean LANG=En CFG=Debug   PLAT=Win32
+nmake.exe /ls Clean LANG=It CFG=Debug   PLAT=Win32
+nmake.exe /ls Clean LANG=Sl CFG=Debug   PLAT=Win32
+nmake.exe /ls Clean LANG=En CFG=Debug   PLAT=x64
+nmake.exe /ls Clean LANG=It CFG=Debug   PLAT=x64
+nmake.exe /ls Clean LANG=Sl CFG=Debug   PLAT=x64
+nmake.exe /ls Clean LANG=En CFG=Release PLAT=Win32
+nmake.exe /ls Clean LANG=It CFG=Release PLAT=Win32
+nmake.exe /ls Clean LANG=Sl CFG=Release PLAT=Win32
+nmake.exe /ls Clean LANG=En CFG=Release PLAT=x64
+nmake.exe /ls Clean LANG=It CFG=Release PLAT=x64
+nmake.exe /ls Clean LANG=Sl CFG=Release PLAT=x64
