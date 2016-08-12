7a459c8c6c785bffb63a73219998a57f8d610de5
Simon Rozman <simon@rozman.si>
2016-08-12 13:19:06 +0200

    Error messages and action text translations imported

--- UI/build_all.bat
+++ UI/build_all.bat
@@ -1,13 +1,5 @@
 @echo off
 nmake.exe /ls All LANG=En CFG=Debug   PLAT=Win32
-nmake.exe /ls All LANG=It CFG=Debug   PLAT=Win32
-nmake.exe /ls All LANG=Sl CFG=Debug   PLAT=Win32
 nmake.exe /ls All LANG=En CFG=Debug   PLAT=x64
-nmake.exe /ls All LANG=It CFG=Debug   PLAT=x64
-nmake.exe /ls All LANG=Sl CFG=Debug   PLAT=x64
 nmake.exe /ls All LANG=En CFG=Release PLAT=Win32
-nmake.exe /ls All LANG=It CFG=Release PLAT=Win32
-nmake.exe /ls All LANG=Sl CFG=Release PLAT=Win32
 nmake.exe /ls All LANG=En CFG=Release PLAT=x64
-nmake.exe /ls All LANG=It CFG=Release PLAT=x64
-nmake.exe /ls All LANG=Sl CFG=Release PLAT=x64
